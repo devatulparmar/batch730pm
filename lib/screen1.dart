67ff89950a8c997c1f5f06f0cc7190237dcb9c74
devatulparmar <dev.atulparmar@gmail.com>
2024-07-02 20:29:56 +0530

    drawer, endDrawer, GestureDetector,InkWell, TextButton

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -17,6 +17,208 @@ class _Screen1State extends State<Screen1> {
       appBar: AppBar(
         title: Text('Batch 7:30 PM'),
       ),
+      endDrawer: Drawer(
+        // width: 500,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            GestureDetector(
+              onTap: (){
+                print('clicked here');
+              },
+              child: Container(
+                color: Colors.lightBlue,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text('Menu 1'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            InkWell(
+              onTap: (){
+                print('clicked inkwell');
+              },
+
+              child: Container(
+                // color: Colors.lightBlue,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text('Menu 1'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text('Menu 1'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              // color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        print('Clicked');
+                      },
+                      child: Text(
+                        'Menu button',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text('Menu 1'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        // width: 500,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            GestureDetector(
+              onTap: (){
+                print('clicked here');
+              },
+              onDoubleTap: (){
+                print('on double tap');
+              },
+
+              child: Container(
+                color: Colors.lightBlue,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text('Menu 1'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            InkWell(
+              onTap: (){
+                print('clicked inkwell');
+              },
+
+              child: Container(
+                // color: Colors.lightBlue,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text('Menu 1'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text('Menu 1'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              // color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        print('Clicked');
+                      },
+                      child: Text(
+                        'Menu button',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.lightBlue,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text('Menu 1'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
       body: ListView(
         children: [
           Center(
