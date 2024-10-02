c0e2b0dd5df3f90ed8242b9c335f03dc0647c733
devatulparmar <dev.atulparmar@gmail.com>
2024-10-02 20:08:52 +0530

    Firebase setup project for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:batch730pm/utils/const.dart';
 import 'package:batch730pm/utils/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try{
+    await Firebase.initializeApp();
+  } catch(e){
+    print(e);
+  }
+
   return runApp(
     MaterialApp(
       navigatorKey: globalNavigatorKey,
