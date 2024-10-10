09c4a9b1aae88f112887db0bb81b1685b14ec7af
devatulparmar <dev.atulparmar@gmail.com>
2024-10-10 19:59:46 +0530

    PushNotificationService in ios (can not complete without developer account)

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,15 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp(
-      name: 'batch730pm',
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyAQhqapx5_Thf7uV-OA05UTjGj8TSfV-N0",
-          appId: "1:665381692579:android:360fc23be9674a7a87cb16",
-          messagingSenderId: '',
-          projectId: "batch730pm-20747",
-      ),
-    );
-    await NotificationService().init();
-    if (Platform.isAndroid) {
-      await NotificationService().isAndroidPermissionGranted();
-    }
-    await NotificationService().requestPermissions();
-    await NotificationService().configureDidReceiveLocalNotificationSubject();
-    await NotificationService().configureSelectNotificationSubject();
-    await PushNotificationService().setupInteractedMessage();
-  } catch (e) {
-    rethrow;
+  await Firebase.initializeApp();
+  await NotificationService().init();
+  if (Platform.isAndroid) {
+    await NotificationService().isAndroidPermissionGranted();
   }
+  await NotificationService().requestPermissions();
+  await NotificationService().configureDidReceiveLocalNotificationSubject();
+  await NotificationService().configureSelectNotificationSubject();
+  await PushNotificationService().setupInteractedMessage();
 
   return runApp(
     MaterialApp(
